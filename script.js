b5bdff3f5a0e63a37367e3a43042f629ad1682da
shivrajsingh123 <45819719+shivrajsingh123@users.noreply.github.com>
2025-04-10 10:30:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const sounds = ['sound1', 'sound2', 'sound3'];
+const audioElements = {};
+
+sounds.forEach(sound => {
+  const audio = new Audio(`sounds/${sound}.mp3`);
+  audioElements[sound] = audio;
+});
+
+document.querySelectorAll('.btn').forEach(button => {
+  button.addEventListener('click', () => {
+    const sound = button.dataset.sound;
+    stopAllSounds();
+    audioElements[sound].currentTime = 0;
+    audioElements[sound].play();
+  });
+});
+
+document.querySelector('.stop').addEventListener('click', () => {
+  stopAllSounds();
+});
+
+function stopAllSounds() {
+  Object.values(audioElements).forEach(audio => {
+    audio.pause();
+    audio.currentTime = 0;
+  });
+}
